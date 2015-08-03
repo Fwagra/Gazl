6704e69b426ef955073c12b540acc81e3350042d
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-08-03 19:30:37 +0200

    Migration:users new fields

---
+++ database/migrations/2015_08_03_172139_add_validation_and_first_name.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddValidationAndFirstName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name');
+            $table->boolean('validated')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('first_name');
+        });
+    }
+}
