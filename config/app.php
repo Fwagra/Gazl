376f86d894eaa2875d355fd437e679e902652b86
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-08-04 11:44:05 +0200

    Replace Illuminate by Laravel Collective (illuminate is no longer maintained)

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -193,6 +194,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Form'      => Collective\Html\FormFacade::class,
 
     ],
 
