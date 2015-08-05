8dd84755d6082d59a6990aa4d481ccde135b0da4
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-08-05 13:23:59 +0200

    Projects migration fields

---
+++ database/migrations/2015_08_05_111409_projects_table_creation.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProjectsTableCreation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('public_id')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}
