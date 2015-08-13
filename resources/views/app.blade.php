062d94761de4f4866178b2b5b6326d2513f0b8f1
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-08-13 19:40:27 +0200

    Adding  jquery-ui

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,12 @@
         <title>Dashboard - @yield('title')</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="{{ URL::asset('js/jquery-1.11.3.min.js') }}"></script>
+        <script src="{{ URL::asset('js/jquery-ui.min.js') }}"></script>
         <script src="{{ URL::asset('js/icheck.min.js') }}"></script>
         <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{URL::asset('css/bootflat.css')}}">
         <link rel="stylesheet" href="{{URL::asset('css/flat/red.css')}}">
+        <link rel="stylesheet" href="{{URL::asset('css/jquery-ui.min.css')}}">
         <link rel="stylesheet" href="{{URL::asset('css/styles.css')}}">
     </head>
     <body>
