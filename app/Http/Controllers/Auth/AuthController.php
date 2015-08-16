5bf480d1f07fa913caa83037fe4bf178ae8173dc
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-08-16 18:16:43 +0200

    Delete public_id cookie after login with user account

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -124,4 +124,53 @@ class AuthController extends Controller
     {
         return redirect('/')->withCookie(Cookie::forget('public_id'));
     }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->loginUsername() => 'required', 'password' => 'required',
+        ]);
+
+        if(Cookie::get('public_id') != null){
+            $cookie = Cookie::forget('public_id');
+        }
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        $throttles = $this->isUsingThrottlesLoginsTrait();
+
+        if ($throttles && $this->hasTooManyLoginAttempts($request)) {
+            return $this->sendLockoutResponse($request);
+        }
+
+        $credentials = $this->getCredentials($request);
+
+        if (Auth::attempt($credentials, $request->has('remember')) && isset($cookie)) {
+            return $this->handleUserWasAuthenticated($request, $throttles)->withCookie($cookie);
+        }else{
+            return $this->handleUserWasAuthenticated($request, $throttles); 
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        if ($throttles) {
+            $this->incrementLoginAttempts($request);
+        }
+
+
+
+        return redirect($this->loginPath())
+            ->withInput($request->only($this->loginUsername(), 'remember'))
+            ->withErrors([
+                $this->loginUsername() => $this->getFailedLoginMessage(),
+            ]);
+    }
 }
