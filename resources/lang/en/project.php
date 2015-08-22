bf61ddc6cc508ff0460fac50425ed72ff2751c2d
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-08-22 17:27:35 +0200

    File correction

--- resources/lang/en/project.php
+++ resources/lang/en/project.php
@@ -2,12 +2,8 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | Authentication Language Lines
+    | Project Language Lines
     |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
     |
     */
     'name' => 'Project name',
