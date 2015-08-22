5a69e32628055901698aa3cbb27caed8bc657217
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-08-22 17:28:11 +0200

    Footer lang files creation

---
+++ resources/lang/en/footer.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Footer Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+    'management' => 'Management',
+    'create_project' => 'Create project',
+    'set_global_key' => 'Define the global key',
+];
---
+++ resources/lang/fr/footer.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Footer Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+    'management' => 'Gestion',
+    'create_project' => 'Créer un projet',
+    'set_global_key' => 'Définir la clé globale',
+];
