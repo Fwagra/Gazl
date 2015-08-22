96c6a6550e0f17781091293748eb6ac53de468c5
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-08-22 21:55:44 +0200

    Enter key button

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -10,6 +10,8 @@
 			<dl class="footer-nav">
 				@if(!File::exists(base_path('storage/.encryption_key')))
 					<a href="{{ route('admin.key') }}" class="btn btn-danger btn-block">{{ trans('footer.set_global_key') }}</a>
+				@elseif(Cookie::get('key') == null)
+					<a href="#" class="btn btn-warning btn-block">{{ trans('footer.set_key') }}</a>
 				@endif
 			</dl>
 		@endif
