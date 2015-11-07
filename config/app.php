1b78391029f3f07b074b5876f100ac23e6852674
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2015-11-07 18:17:22 +0100

    Add Html façade

--- config/app.php
+++ config/app.php
@@ -198,6 +198,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
