aa00fae019adef7970faa9aa660e3b3326f11c24
Fabio Cerqueira <fabiocerqueiracw@gmail.com>
2016-01-12 22:41:14 +0100

    Bug reporting - Moving bug list to a proper template

--- resources/views/bugs/index.blade.php
+++ resources/views/bugs/index.blade.php
@@ -3,23 +3,11 @@
 @section('content')
 	<h1>{!! trans('bug.list_h1') !!}</h1>
 	@include('bugs.navbar')
-	@if (count($bugs))
-		<ul class="list-group">
-			@foreach ($bugs as $bug)
-				<li class="list-group-item clearfix">
-					<div class="col-md-1 state">{{ $bug->state }}</div>
-					<div class="col-md-10">
-						<a href="{{ action('BugController@show', [$project->slug, $bug->id]) }}"> {{ $bug->name }}</a>
-						@if ($bug->private == 1)
-							<span class="private">{{ trans('bug.private_bug') }}</span>
-						@endif
-						<div class="bug_date">{{ trans('bug.reported_on') }} {{ $bug->created_at->format('d-m-Y') }}</div>
-					</div>
-					<div class="col-md-1">{{ trans('bug.delete') }}</div>
-				</li>
-			@endforeach
-		</ul>
-	@endif
+	<div class="bug-content">
+		@if (count($bugs))
+			@include('bugs.list')
+		@endif
+	</div>
 	{!! $bugs->appends(Input::except('page'))->render(); !!}
 	
 @endsection
