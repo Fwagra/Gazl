7bfdefd05486990c22b3a429c1c3a6c604750dca
Fabio <fabiocerqueiracw@gmail.com>
2016-04-02 15:54:36 +0200

    Mémo - Migration for project informations field

---
+++ database/migrations/2016_04_02_125817_project_informations.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProjectInformations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+}
--- resources/lang/en/project.php
+++ resources/lang/en/project.php
@@ -11,6 +11,7 @@ return [
     'list_h1' => 'Type a project to search',
     'list_title' => 'Search a project',
     'name' => 'Project name',
+    'no_desc' => "No informations yet",
     'no_results_found' => 'No results found',
     'save' => 'Save the project',
     'search_button' => 'Search',
