4c507fa3b7b9a37e441d9fe8285fbdb3c7828b1e
Fabio <fabiocerqueiracw@gmail.com>
2016-04-02 20:42:41 +0200

    Mémos - Migration file for mémos

---
+++ database/migrations/2016_04_02_140400_create_memo.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('project_id');
+            $table->string('name');
+            $table->boolean('active', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('memos');
+    }
+}
