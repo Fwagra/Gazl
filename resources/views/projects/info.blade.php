583176bad27e2183403f188e743d1e7e95a87ebd
Fabio <fabiocerqueiracw@gmail.com>
2017-03-15 14:51:43 +0100

    Project - Fix project variable

--- resources/views/projects/info.blade.php
+++ resources/views/projects/info.blade.php
@@ -2,21 +2,24 @@
     <span class="label float label-primary" data-toggle="modal" data-target="#code-project-popup">{{ $projectPID }}</span>
 @endif
 @section('footer_js')
+    @if (isset($project))
     <div class="modal fade" id="code-project-popup" tabindex="-1" role="dialog" aria-labelledby="titrePopUp" aria-hidden="true">
-          <div class="modal-dialog">
+        <div class="modal-dialog">
             <div class="modal-content">
-              <div class="modal-header">
-                  <h4 class="modal-title">{!! trans('project.popup_code_title') !!}
-                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> </h4>
-              </div>
-              <div class="modal-body">
-                {!! Form::open(['route' => ['project.sendcode', $project->slug], 'method' => 'POST', 'class' => "form-send-code", 'data-toggle' => 'validator']) !!}
+                <div class="modal-header">
+                <h4 class="modal-title">{!! trans('project.popup_code_title') !!}
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> </h4>
+                </div>
+                <div class="modal-body">
+                    {!! Form::open(['route' => ['project.sendcode', $project->slug], 'method' => 'POST', 'class' => "form-send-code", 'data-toggle' => 'validator']) !!}
                     {!! Form::label(trans('project.email_field')) !!}
                     {!! Form::email('email', '', ['class' => 'form-control', 'data-error' => 'test', 'required' => 'required'])!!}
                     {!! Form::submit(trans('project.send_code_button'), array('class' => 'button expand')) !!}
-                {!! Form::close() !!}
-              </div>
+                    {!! Form::close() !!}
+                </div>
             </div>
-          </div>
         </div>
+    </div>
+    @endif
+
 @endsection
